37c0e330c79c6ff24781bf55ac89a8bb43bbc325
CodingPhase <joesantosgarcia@gmail.com>
2023-04-06 15:25:07 -0400

    testing

--- public/index.php
+++ public/index.php
@@ -6,10 +6,12 @@ use Core\DBConnection;
 require '../vendor/autoload.php';
 require '../core/helpers.php';
 require '../app/routes.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../' );
-if($_ENV['APP_ENV'] !== 'production') {
+if (file_exists(__DIR__ . '/../' . '.env')) {
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../' );
   $dotenv->load(__DIR__);
 }
+
+
 // require '../app/views/index.view.php';
 
 // $EmailModel = new EmailModel(DBConnection::start());
