eb9d8ff27dbecfb3d100cdeabd5c77d7fb91def9
CodingPhase <joesantosgarcia@gmail.com>
2023-04-06 13:51:27 -0400

    added env file with passwords

--- public/index.php
+++ public/index.php
@@ -2,9 +2,12 @@
 use App\Models\EmailModel;
 use Core\DBConnection;
 
+
 require '../vendor/autoload.php';
 require '../core/helpers.php';
 require '../app/routes.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__) );
+$dotenv->load();
 // require '../app/views/index.view.php';
 
 // $EmailModel = new EmailModel(DBConnection::start());
